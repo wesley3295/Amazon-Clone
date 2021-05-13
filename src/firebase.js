bda21f4bf2d459b10b57a151c1b9e81d973426ca
Wesley <wspencer3295@yahoo.com>
2021-05-13 16:15:01 -0700

    initial

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCep1PCQZO6WbthgKKen3aT96EKxVuXGF0",
+    authDomain: "clone-22f24.firebaseapp.com",
+    projectId: "clone-22f24",
+    storageBucket: "clone-22f24.appspot.com",
+    messagingSenderId: "800743443489",
+    appId: "1:800743443489:web:06a34395ebc2d0c4ede760"
+  };
