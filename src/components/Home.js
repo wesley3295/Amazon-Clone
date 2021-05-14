38e24c06706b0e5a5f6c38ea15e24b9dec0f431e
Wesley <wspencer3295@yahoo.com>
2021-05-14 15:57:04 -0700

    added home comp & css

---
+++ src/components/Home.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import '../CSS/Home.css'
+import Product from './Product'
+const Home = () => {
+    return (
+        <div className="home">
+            <div className="home__conatainer">
+                <img className="home__image" src="https://images-na.ssl-images-amazon.com/images/G/01/digital/video/merch/2021/Other/RB-3968_SVOD_MultiTitle_GW_Test/AMZN_RB-3968_SVOD_MultiTitle_GW_Control_EN_GWBleedingHero_1500x600._CB654815209_.jpg" alt="Banner" />
+                <div className="home__row">
+                    <Product title="The Lean Startup"  image="https://images-na.ssl-images-amazon.com/images/I/81-QB7nDh4L.jpg" price="29.99" rating={3}/>
+                    <Product title="The Lean Startup"  image="https://images-na.ssl-images-amazon.com/images/I/81-QB7nDh4L.jpg" price="29.99" rating={3}/>
+                </div>
+                <div className="home__row">
+                <Product title="The Lean Startup"  image="https://images-na.ssl-images-amazon.com/images/I/81-QB7nDh4L.jpg" price="29.99" rating={3}/>
+                <Product title="The Lean Startup"  image="https://images-na.ssl-images-amazon.com/images/I/81-QB7nDh4L.jpg" price="29.99" rating={3}/>
+                <Product title="The Lean Startup"  image="https://images-na.ssl-images-amazon.com/images/I/81-QB7nDh4L.jpg" price="29.99" rating={3}/>
+
+                </div>
+                <div className="home__row">
+                <Product title="The Lean Startup"  image="https://images-na.ssl-images-amazon.com/images/I/81-QB7nDh4L.jpg" price="29.99" rating={3}/>
+
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Home
