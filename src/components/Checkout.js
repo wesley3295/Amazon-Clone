3cf8f951c8a226f6937b2d7224d276b7e955494b
Wesley <wspencer3295@yahoo.com>
2021-05-15 15:54:12 -0700

    checkout component

---
+++ src/components/Checkout.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import '../CSS/Checkout.css'
+import Subtotal from './Subtotal'
+const Checkout = () => {
+    return (
+        <div className="checkout">
+            <div className="checkout__left">
+                <img src="https://images-na.ssl-images-amazon.com/images/G/02/UK_CCMP/TM/OCC_Amazon1" alt="" className="checkout__ad" />
+                <div>
+                    <h2 className="checkout__title">
+                        Your Shopping Basket
+               </h2>
+                </div>
+            </div>
+            <div className="checkout__right">
+                <Subtotal/>
+            </div>
+        </div>
+    )
+}
+
+export default Checkout
