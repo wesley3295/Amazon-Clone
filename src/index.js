b699aabce237beca9bb4fb9d704492be66d107a5
Wesley <wspencer3295@yahoo.com>
2021-05-16 16:04:53 -0700

    adding state managment to index

--- src/index.js
+++ src/index.js
@@ -3,8 +3,8 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import * as serviceWorker from "./serviceWorker"
-import StateProvider from './StateProvider'
+// import * as serviceWorker from "./serviceWorker"
+import {StateProvider} from './StateProvider'
 import reducer, {initialState} from './reducer'
 ReactDOM.render(
   <React.StrictMode>
