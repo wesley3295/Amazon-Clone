4c8bd28f22453ad900c0f9f658b9aeec4e669561
Wesley <wspencer3295@yahoo.com>
2021-05-16 16:05:11 -0700

    add reducer

--- src/reducer.js
+++ src/reducer.js
@@ -1,14 +1,35 @@
 export const initialState = {
     basket:[],
+    value:0,
 }
 
 const reducer = (state, action) => {
+    console.log("action:", action)
     switch (action.type) {
-        case 'ADD_TO_BASKET':
-            return{
+        case "ADD_TO_BASKET":
+            return {
                 ...state,
-                basket:[...state.basket,action.item],
+                basket:[...state.basket, action.item],
+                value:state.value+parseFloat(action.item.price)
             };
+            case "REMOVE_FROM_BASKET":
+                const index = state.basket.findIndex(
+                    (basketItem) => basketItem.id === action.id
+                    )
+                    let newBasket = [...state.basket]
+                    if (index>= 0){
+                        newBasket.splice(index,1)
+                        
+                }else{
+                    console.warn(`Cant remove product (id:${action.id}) as it's not in basket!`)
+                }
+                return {
+                    ...state,
+                    basket:newBasket,
+                    value:state.value - parseFloat(action.price)
+                }
+            default:
+                return state;
     }
 };
 export default reducer
