6a3517d1f2047b2562f3f1923719f4f6f4cc8db2
Wesley <wspencer3295@yahoo.com>
2021-05-16 16:07:50 -0700

    dispatch action to reducer to update global state basket

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,6 +1,22 @@
 import React from 'react'
 import '../CSS/Product.css'
-const Product = ({id, title, image,price,rating}) => {
+import { useStateValue } from "../StateProvider"
+const Product = ({ id, title, image, price, rating }) => {
+    const [{ basket,value }, dispatch] = useStateValue()
+    console.log("Basket:",basket)
+    console.log("Value:",value)
+    const addToBasket = () => {
+        dispatch({
+            type: 'ADD_TO_BASKET',
+            item: {
+                id: id,
+                title: title,
+                image: image,
+                price: price,
+                rating: rating,
+            }
+        })
+    }
     return (
         <div className="product">
             <div className="product__info">
@@ -10,13 +26,13 @@ const Product = ({id, title, image,price,rating}) => {
                     <small>{price}</small>
                 </p>
                 <div className="product__rating">
-                {Array(rating).fill().map((_,i)=>{
-                    return <p>🌟</p>
-                })}
+                    {Array(rating).fill().map((_, i) => {
+                        return <p>🌟</p>
+                    })}
                 </div>
             </div>
-                <img src={image} alt=""/>
-                <button className="product__button">Add to Basket</button>
+            <img src={image} alt="" />
+            <button onClick={addToBasket} className="product__button">Add to Basket</button>
         </div>
     )
 }
