4acf01c382bac1f744baaee1d9c2f86057917f8d
Wesley <wspencer3295@yahoo.com>
2021-05-24 22:29:50 -0700

    add user login

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,10 +4,15 @@ import SearchIcon from "@material-ui/icons/Search";
 import ShoppingBasketIcon from "@material-ui/icons/ShoppingBasket";
 import {Link} from 'react-router-dom'
 import { useStateValue } from "../StateProvider"
+import { auth } from "../firebase";
 
 const Header = () => {
-  const [{ basket }, dispatch] = useStateValue()
-
+  const [{ basket,user }, dispatch] = useStateValue()
+const handleAuthentication=()=>{
+  if(user){
+    auth.signOut()
+  }
+}
   return (
     <div className="header">
       <Link to="/"> <img
@@ -22,10 +27,10 @@ const Header = () => {
       </div>
 
       <div className="header__nav">
-          <Link to="/login">
-        <div className="header__option">
-          <span className="header__optionLineOne">Hello</span>
-            <span className="header__optionLineTwo">Sign In</span>
+          <Link to={!user&&"/login"}>
+        <div onClick={handleAuthentication} className="header__option">
+          <span className="header__optionLineOne">{user&&'Hello Guest'}</span>
+            <span className="header__optionLineTwo">{user?"Sign Out":"Sign In"}</span>
         </div>
           </Link>
 
