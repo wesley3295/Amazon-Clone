f44ad3d1e9e11f4ac1bdbb20f45695e1718cdb43
Wesley <wspencer3295@yahoo.com>
2021-05-27 16:09:11 -0700

    update to order bug in Orders db

--- src/components/Orders.js
+++ src/components/Orders.js
@@ -1,11 +1,47 @@
-import React from 'react'
-import '../CSS/Orders.css'
-const Orders = () => {
-    return (
-        <div className="orders">
-            <h1>Your Orders</h1>
-        </div>
-    )
+import React, { useState, useEffect } from "react";
+// import {db} from "../firebase";
+import "../CSS/Orders.css";
+import { useStateValue } from "../StateProvider";
+import Order from "./Order";
+
+function Orders({db}) {
+  const [{ basket, user }, dispatch] = useStateValue();
+  const [orders, setOrders] = useState([]);
+    useEffect(() => {
+      if (user) {
+        db.collection("users")
+          .doc(user?.uid)
+          .collection("orders")
+          .orderBy("created", "desc")
+          .onSnapshot((snapshot) =>
+            setOrders(
+              snapshot.docs.map((doc) => ({
+                id: doc.id,
+                data: doc.data(),
+              }))
+            )
+          );
+      } else {
+        setOrders([]);
+      }
+    }, [user]);
+
+  return (
+    <div className="orders">
+      <h1>Your Orders</h1>
+
+      <div className="orders__order">
+        {orders.map((order) => {
+          return (
+            <>
+              {console.log("hello?", order)}
+              <Order order={order} />
+            </>
+          );
+        })}
+      </div>
+    </div>
+  );
 }
 
-export default Orders
+export default Orders;
