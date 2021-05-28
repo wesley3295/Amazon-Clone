f1ba9fc7e43fe0b215783cb2dc2a78d20fd7864f
Wesley <wspencer3295@yahoo.com>
2021-05-28 14:20:26 -0700

    fixed api key

--- src/firebase.js
+++ src/firebase.js
@@ -1,18 +1,19 @@
 import firebase from "firebase"
+require('dotenv').config()
 
 
 const firebaseConfig = {
-    apiKey: process.env.API_KEY,
-    authDomain: "clone-22f24.firebaseapp.com",
-    projectId: "clone-22f24",
-    storageBucket: "clone-22f24.appspot.com",
-    messagingSenderId: "800743443489",
-    appId: "1:800743443489:web:06a34395ebc2d0c4ede760"
-  };
-  
-  const firebaseApp = firebase.initializeApp(firebaseConfig);
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: "clone-22f24.firebaseapp.com",
+  projectId: "clone-22f24",
+  storageBucket: "clone-22f24.appspot.com",
+  messagingSenderId: "800743443489",
+  appId: "1:800743443489:web:06a34395ebc2d0c4ede760"
+};
 
-  const db = firebaseApp.firestore();
-  const auth = firebase.auth();
-  
-  export { db,auth };
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+
+const db = firebaseApp.firestore();
+const auth = firebase.auth();
+
+export { db, auth };
