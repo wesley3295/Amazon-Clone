d5c4d581489afd2687ac17f170b9c690a7a5ee94
Wesley <wspencer3295@yahoo.com>
2021-05-28 14:21:27 -0700

    activated cloud function api endpoint for axios

--- src/async/Axios.js
+++ src/async/Axios.js
@@ -1,7 +1,10 @@
 import axios from 'axios'
 
 const instance = axios.create({
-    baseURL://'https://us-central1-clone-22f24.cloudfunctions.net/api'
-    'http://127.0.0.1:5001/clone-22f24/us-central1/api' // The api (cloud function) url
+    baseURL:
+    //deploying
+    'https://us-central1-clone-22f24.cloudfunctions.net/api'// The api (cloud function) url
+    //testing
+    //  'http://127.0.0.1:5001/clone-22f24/us-central1/api' 
 });
 export default instance
